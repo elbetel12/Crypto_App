a163cfe77e10a57c8134d98960584597b8b6fe43
Elbetel Molla <elbetelmolla25@gmail.com>
2024-09-18 11:18:13 +0300

    fix error

--- crypto_app/src/ThemeContext.tsx
+++ crypto_app/src/ThemeContext.tsx
@@ -5,10 +5,10 @@ const ThemeContext = createContext<any>(null);
 export const useTheme = () => useContext(ThemeContext);
 
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
 
   const toggleTheme = () => {
-    setIsDarkMode((prevMode) => !prevMode);
+    setIsDarkMode((prevMode: boolean) => !prevMode);
   };
 
   return (
